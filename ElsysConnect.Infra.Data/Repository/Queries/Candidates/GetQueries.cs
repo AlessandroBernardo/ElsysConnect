9ae0e04aa04257d6bcfefcc5ef657374ca46b2b6
Alessandro Bernardo da SIlveira <alessandro.silveira@ltm.digital>
2023-02-27 14:17:06 -0300

    alterações

--- ElsysConnect.Infra.Data/Repository/Queries/Candidates/GetQueries.cs
+++ ElsysConnect.Infra.Data/Repository/Queries/Candidates/GetQueries.cs
@@ -1,15 +1,32 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data.SqlClient;
 
 namespace ElsysConnect.Infra.Data.Repository.Queries.Candidates
 {
-    public class GetQueries
-    {
+    public class GetQueries    {
+
+        //private readonly IConfiguration _configuration;
+
+        //public GetQueries()
+        //{
+
+        //}
+
+        //public GetQueries(IConfiguration configuration)
+        //{
+        //    _configuration = configuration;
+        //}
+  
         public static string CandidatesRecruitmentPhases()
         {
+            //var connectionString = _configuration.GetConnectionString("ConnectionDefault");
+            //string catalog = new SqlConnectionStringBuilder(connectionString).InitialCatalog;
+
             return $@"SELECT c.Id, c.Name as CandidateName, c.Email as CandidateEmail, c.Phone as CandidatePhone, jp.Title as JobPositionTitle, rp.Name as PhaseName, crp.Status
-            FROM [ElsysConnectDb].[dbo].[CandidateRecruitmentPhase] crp
+            FROM ElsysConnectDbMigrationTeste.[dbo].[CandidateRecruitmentPhase] crp
             LEFT JOIN Candidates c ON crp.CandidateId = c.Id
             LEFT JOIN JobPositionRecruitmentPhase jprp ON jprp.Id = crp.JobPositionRecruitmentPhaseId
             LEFT JOIN JobPositions jp ON jp.Id = jprp.JobPositionId
